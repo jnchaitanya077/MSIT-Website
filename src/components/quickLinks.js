e651641ddb680342283328f4777e6a36df7276fc
ranjith210297 <ranjeeth2197@msitprogram.net>
2020-06-12 12:48:56 +0530

    react files added

---
+++ src/components/quickLinks.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInfo,
+  faGraduationCap,
+  faRupeeSign,
+  faIndustry,
+  faCheck,
+} from "@fortawesome/free-solid-svg-icons";
+import "../css/quickLinks.css";
+
+function App() {
+  return (
+    <div className="row" style={{ marginRight: 0, marginLeft: 0 }}>
+      <div className="col-md-6 col-lg links-alignment">
+        <FontAwesomeIcon icon={faInfo} size="3x" className="quick-icons" />
+        <Link to="/msit-vision" className="links">
+          <h3>MSIT Vision</h3>
+        </Link>
+      </div>
+      <div className="col-md-6 col-lg links-alignment">
+        <FontAwesomeIcon
+          icon={faGraduationCap}
+          size="3x"
+          className="quick-icons"
+        />
+        <h3>Specializations</h3>
+      </div>
+      <div className="col-md-6 col-lg links-alignment">
+        <FontAwesomeIcon icon={faRupeeSign} size="3x" className="quick-icons" />
+        <h3>Fee Structure</h3>
+      </div>
+      <div className="col-md-6 col-lg links-alignment">
+        <FontAwesomeIcon icon={faIndustry} size="3x" className="quick-icons" />
+        <h3>Placements</h3>
+      </div>
+      <div className="col-md-12 col-lg links-alignment">
+        <FontAwesomeIcon icon={faCheck} size="3x" className="quick-icons" />
+        <h3>Apply</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
