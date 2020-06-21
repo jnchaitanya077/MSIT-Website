4e62c3e817ed394f71b2a225cf06ee439357b4a9
Pranay Kumar Reddy Goli <golipranayreddy@msitprogram.net>
2020-06-21 18:20:47 +0530

    Changes to UI (MSIT Program page)

--- src/AboutMsit/MsitProgram.js
+++ src/AboutMsit/MsitProgram.js
@@ -9,68 +9,41 @@ function App(){
     return(
         <div>
             <HeaderTitle name = "Overview"/>
-            <div className="container about-MsitProgram Justify-content-center" style={{paddingTop: "40px", paddingBottom:"40px"}}>
+            <div className="container about-MsitProgram Justify-content-center" style={{paddingTop: "40px"}}>
                 <div className = "col news" id = "charter" style={{height:"auto", padding:"40px" }}>
-                    <div className="col-12" id ="para">
-                        <h1 style={{color:"rgb(119, 11, 11)", paddingBottom:"10px"}}>Msit Charter</h1>
-                        <p>To prepare very high quality IT professionals for a global market by grooming them in IT and Soft Skills.</p>
-                        <p>To enable them to accept opportunities in a framework of professionally sound and quality conscious organizations, as well as to take up entrepreneurial ventures.</p>
-                        <p>To develop their sensitivity and capability to continually respond to changing technology and working environments, by using an 'active learning' approach.</p>
-                        <p>To provide an opportunity not only to IT professionals but also to professionals and experts from non-IT domains to acquire a thorough knowledge of IT concepts and the capacity to swiftly translate this knowledge into services in their respective domains.</p>
-                    </div>
+                    <h1 style={{color:"rgb(119, 11, 11)", paddingBottom:"10px"}}>Msit Charter</h1>
+                    <p>To prepare very high quality IT professionals for a global market by grooming them in IT and Soft Skills.</p>
+                    <p>To enable them to accept opportunities in a framework of professionally sound and quality conscious organizations, as well as to take up entrepreneurial ventures.</p>
+                    <p>To develop their sensitivity and capability to continually respond to changing technology and working environments, by using an 'active learning' approach.</p>
+                    <p>To provide an opportunity not only to IT professionals but also to professionals and experts from non-IT domains to acquire a thorough knowledge of IT concepts and the capacity to swiftly translate this knowledge into services in their respective domains.</p>
                 </div>
-
-
                 <div className="col news" id = "MsitProgram" style = {{height:"auto", padding:"40px" }}>
-                    <div className="row profile-row">
-                        {/* <!-- text column--> */}
-                        <div className="col-7" >
-                            <h1 style={{color:"rgb(119, 11, 11)"}}>MSIT Program</h1>
-                            <p>MSIT program is being offered by “Consortium of Institutions of Higher Learning” (CIHL), formed by the universities in Andhra Pradesh and Telangana. CIHL has designed the program with inputs from major IT companies</p>
-                            &nbsp;
-                            &nbsp;
-                            <h4>This is a glimpse of the leading institutions involved in this programme:</h4>
-                            <div className="col news" style={{height:"auto" , paddingTop:"2%",paddingBottom:"1%", marginTop:"20px", backgroundColor:"#f0f0f5"}}>
-                                <ul>
-                                    <li>IIIT, Hyderabad (IIIT-H)</li>
-                                    <li>JNTU, Hyderabad (JNTUH)</li>
-                                    <li>JNTU, Kakinada (JNTUK)</li>
-                                    <li>JNTU, Anantapur (JNTUA)</li>
-                                    <li>SV University, Tirupati (SVU)</li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        {/* <!-- image column--> */}
-                        <div className="col-5" style={{paddingLeft:"40px", paddingTop:"40px"}}>
-                            <div  className="col news" style={{width:"95%", height :"auto", paddingTop:"1%", paddingLeft:"1%", paddingRight:"0%", paddingBottom:"1%",  marginTop:"9%"}}>
-                                <img src={MsitAdv} alt="" className="alignright" style={{width: "99%"}}/>
-                            </div>
-                        </div>
-                        
+                    <h1 style={{color:"rgb(119, 11, 11)"}}>MSIT Program</h1>
+                    <img src={MsitAdv} style={{width: "38%", float:"right", paddingLeft:"2%"}}/>
+                    <p>MSIT program is being offered by “Consortium of Institutions of Higher Learning” (CIHL), formed by the universities in Andhra Pradesh and Telangana. CIHL has designed the program with inputs from major IT companies</p>  
+                    <h4>This is a glimpse of the leading institutions involved in this programme:</h4>
+                    <div className="col news" style={{height:"auto", width:"500px" , padding:"2%", marginTop:"2%"}}>
+                        <ul>
+                            <li>IIIT, Hyderabad (IIIT-H)</li>
+                            <li>JNTU, Hyderabad (JNTUH)</li>
+                            <li>JNTU, Kakinada (JNTUK)</li>
+                            <li>JNTU, Anantapur (JNTUA)</li>
+                            <li>SV University, Tirupati (SVU)</li>
+                        </ul>
                     </div>
-
-                    <div className="row profile-row">
-                        {/* <!-- text column--> */}
-                        <div className="col-12">
-                            <p>MSIT aims to produce skilled Professionals who have knowledge of the core principles underlying information technology and an understanding of emerging industry trends.The MSIT program was started in 2001 and students were admitted to the Learning Centers at International Institute of Information Technology (IIIT), Hyderabad, School of IT, Jawaharlal Nehru Technological University (JNTU), Hyderabad, College of Engineering, Osmania University and Regional Engineering College (REC, now NIT), Warangal.</p>
-                            <p>The MSIT (MS in IT or Master of Science in Information Technology) is a two year post graduate programme in Computer Science. MSIT is an innovative multi-university interdisciplinary post-graduate programme. MSIT program is being offered by “Consortium of Institutions of Higher Learning” (CIHL), formed by the Universities.</p>
-                            <p>In 2004, the Story/Problem Centered Curriculum was introduced and the fourth batch of students were admitted to the MSIT division, International Institute of Information Technology (IIIT), College of Engineering, Osmania University (OU), Hyderabad and School of Information Technology (SIT), Jawaharlal Nehru Technological University (JNTU).</p>
-                            <p>MSIT offers the latest domain specializations on par with international universities. Computer Networks and Information Security as domain specializations are offered for the first time in the states (AP & Telangana) only in MSIT. Besides these, the course also offers specializations in Software Engineering and E-commerce which have very high demand in the current software industry.</p>        
-
-                            <h4>Eligibility : <span style={{color: "red",fontSize :"large"}}> B.Tech / B.E (All Branches)</span></h4>
-                            <h3 style={{margin:"0%", marginBottom:"1%"}}>Want to know about Specializations?</h3>
-                            <Link to = "/CoursesAndStructure/#specializations" className="btn btn-outline-success" >
-                                Click here
-                            </Link>
-                        </div>
-
-                    </div>
-
-
+                    <p>MSIT aims to produce skilled Professionals who have knowledge of the core principles underlying information technology and an understanding of emerging industry trends.The MSIT program was started in 2001 and students were admitted to the Learning Centers at International Institute of Information Technology (IIIT), Hyderabad, School of IT, Jawaharlal Nehru Technological University (JNTU), Hyderabad, College of Engineering, Osmania University and Regional Engineering College (REC, now NIT), Warangal.</p>
+                    <p>The MSIT (MS in IT or Master of Science in Information Technology) is a two year post graduate programme in Computer Science. MSIT is an innovative multi-university interdisciplinary post-graduate programme. MSIT program is being offered by “Consortium of Institutions of Higher Learning” (CIHL), formed by the Universities.</p>
+                    <p>In 2004, the Story/Problem Centered Curriculum was introduced and the fourth batch of students were admitted to the MSIT division, International Institute of Information Technology (IIIT), College of Engineering, Osmania University (OU), Hyderabad and School of Information Technology (SIT), Jawaharlal Nehru Technological University (JNTU).</p>
+                    <p>MSIT offers the latest domain specializations on par with international universities. Computer Networks and Information Security as domain specializations are offered for the first time in the states (AP & Telangana) only in MSIT. Besides these, the course also offers specializations in Software Engineering and E-commerce which have very high demand in the current software industry.</p>        
+                    <h4>Eligibility : <span style={{color: "red",fontSize :"large"}}> B.Tech / B.E (All Branches)</span></h4>
+                    <h3 style={{margin:"0%", marginBottom:"1%"}}>Want to know about Specializations?</h3>
+                    <Link to = "/CoursesAndStructure/#specializations" className="btn btn-outline-success" >
+                        Click here
+                    </Link>
                 </div> 
             </div>
         </div>
     );
 }
 export default App;
+
