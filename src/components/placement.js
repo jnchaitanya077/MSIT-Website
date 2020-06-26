c3e43eef1c31bc96a1077d1ed7d174b0b3932737
jnchaitanya077 <55684213+jnchaitanya077@users.noreply.github.com>
2020-06-26 13:33:18 +0530

    placements-image added

--- src/components/placement.js
+++ src/components/placement.js
@@ -3,24 +3,21 @@ import "../css/placement.css";
 
 function App() {
   return (
-    <div>
-      <div className="image"></div>
-      <div className="row placement">
-        <div className="col-sm placement-heading">
-          <h2>
-            MSIT Alumni Work in Leading
-            <br /> Organizations Of the World.
-          </h2>
-          <div className="horizontal"></div>
-          <p className="general-text placement-body">
-            Year 2018 – 11 students placed with a salary package in excess
-            <br /> of 20 Lakhs Per Annum
-            <br />
-            <button type="button" className="btn btn-success .btn-sm mt-3">
-              View More
-            </button>
-          </p>
-        </div>
+    <div className="row placement">
+      <div className="col-sm placement-heading md">
+        <h2>
+          MSIT Alumni Work in Leading
+          <br /> Organizations Of the World.
+        </h2>
+        <div className="horizontal"></div>
+        <p className="general-text placement-body">
+          Year 2018 – 11 students placed with a salary package in excess
+          <br /> of 20 Lakhs Per Annum
+          <br />
+          <button type="button" className="btn btn-success .btn-sm mt-3">
+            View More
+          </button>
+        </p>
       </div>
     </div>
   );
